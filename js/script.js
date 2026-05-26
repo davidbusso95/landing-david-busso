def29d7013fec97f1c18968b146e2e8336a8acb4
David <david.busso95@gmail.com>
2026-05-26 14:23:04 -0300

    Initial landing page upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Meta Pixel 
+
+!function(f,b,e,v,n,t,s){
+      if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)
+    }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '830580196338739');
+    fbq('track', 'PageView');
+
+const cards = document.querySelectorAll('.card');
+
+  cards.forEach((card) => {
+    card.addEventListener('mousemove', (e) => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      card.style.setProperty('--x', `${x}px`);
+      card.style.setProperty('--y', `${y}px`);
+    });
+
+    card.addEventListener('mouseleave', () => {
+      card.style.removeProperty('--x');
+      card.style.removeProperty('--y');
+    });
+  });
